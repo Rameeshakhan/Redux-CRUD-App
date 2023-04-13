952a7fdfff759992fe7b81bcda90fcc44b32ad30
Rameesha Khan <rameeshakhan75@gmail.com>
2023-04-14 01:03:01 +0500

    AddUser Route Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
+import AddUser from './pages/AddUser'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/adduser" element={<AddUser />} />
       </Routes>
 
     </div>
