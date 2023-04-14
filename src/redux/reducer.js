933e912b4766a8c3497cba7608d59f394454abe5
Rameesha Khan <rameeshakhan75@gmail.com>
2023-04-15 02:00:45 +0500

    Delete Switch case added

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -14,6 +14,11 @@ const usersReducer = (state = initialState, action) => {
             users: action.payload,
             loading: false,
         }
+        case types.deleteUser:
+            return{
+                ...state,
+                loading: false,
+            }
         default:
             return state;
     }
