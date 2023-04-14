4293fc3630d6689028ba42a6750cc3cba258dadc
Rameesha Khan <rameeshakhan75@gmail.com>
2023-04-15 01:59:27 +0500

    Delete Action Created

--- src/redux/action.js
+++ src/redux/action.js
@@ -6,12 +6,27 @@ const getUsers = (users) =>({
     payload: users,
 })
 
+const deleteU = () => ({
+    type: types.deleteUser,
+})
+
 export const loadUsers = () => {
     return function (dispatch) {
         axios.get(`http://localhost:5000/users`)
         .then((res) => {
-            console.log(res.data)
             dispatch(getUsers(res.data))
+        })
+        .catch(err => console.log("err" + err))
+    }
+}
+
+export const deleteUser = (id) => {
+    return function (dispatch) {
+        axios.delete(`http://localhost:5000/users/${id}`)
+        .then((res) => {
+            // console.log(res.data)
+            dispatch(deleteU())
+            dispatch(loadUsers())
         })
         .catch(err => console.log("err" + err))
     }
