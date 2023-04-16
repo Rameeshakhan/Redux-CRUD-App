ac47ffc3b2fb9f3eef39c0682b99ae4d8e8a03a0
Rameesha Khan <rameeshakhan75@gmail.com>
2023-04-17 03:36:06 +0500

    Update Action type defined

--- src/redux/actionType.js
+++ src/redux/actionType.js
@@ -1,3 +1,5 @@
 export const getUser = "GET_USER";
 export const deleteUser = "DELETE_USER";
 export const addUser = "ADD_USER";
+export const getSingleUser = "GET_SINGLE_USER";
+export const updateUser = "UPDATE_USER";
