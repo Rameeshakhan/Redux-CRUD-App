45885ae44e318ccdc88b586ae0f909367541a879
Rameesha Khan <rameeshakhan75@gmail.com>
2023-04-17 03:34:01 +0500

    Edit Route Updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { Routes, Route } from "react-router-dom"
 import Home from './pages/Home'
 import AddUser from './pages/AddUser'
+import EditUser from './pages/EditUser'
 
 const App = () => {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/adduser" element={<AddUser />} />
+        <Route path="/edituser/:id" element={<EditUser />} />
       </Routes>
 
     </div>
